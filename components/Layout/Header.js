5eaea6a2a1be1837a6d37ac61ad752ebd450d4ea
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-12-22 15:20:43 +0530

    cart open and close

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -2,13 +2,14 @@ import React, { Fragment } from "react";
 import melasImage from "../../assets/meals.jpg";
 
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
-export default function Header() {
+export default function Header({ onShowCart }) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>cart</button>
+        <HeaderCartButton onClick={onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={melasImage} alt="A table full of delicious food!"></img>
